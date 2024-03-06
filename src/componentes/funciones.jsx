a07cfeee90eae4367d10801c6b18b5ff4401c489
unknown <alvixkarlozz@hotmail.com>
2024-03-05 20:07:59 -0400

    commit para añadir las funcionalidades del programa

---
+++ src/componentes/funciones.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './page.css'
+
+
+function Header2() {
+
+    return (
+      <>
+        <div>
+        
+         
+        </div>
+      </>
+    )
+  }
+  
+  export default Header2
