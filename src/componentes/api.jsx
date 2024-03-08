a5c78c66605c5f7b8cef230becfacff22d0644b7
unknown <alvixkarlozz@hotmail.com>
2024-03-08 12:39:48 -0400

    Commit correctivo para eliminar la visualizacion del console.log

--- src/componentes/api.jsx
+++ src/componentes/api.jsx
@@ -8,7 +8,6 @@ function Button() {
       fetch('https://prod-25.brazilsouth.logic.azure.com:443/workflows/dc866b2a095d4663a962a344e2e551b7/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=1RIcAOjxcT78W9hNYl6KvodMaObbty8aBeFWzNYToIM')
         .then(response => response.json())
         .then(data => {
-          console.log(data)
           setDatos(data);
         })
         .catch(error => {
